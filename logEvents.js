7de4aba2e2f003f931d58e584c8d722b9d71ccab
Alvin Okafor <alvin2k99@gmail.com>
2023-03-25 21:32:54 +0100

    initial commit

---
+++ logEvents.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+const crypto = require("crypto");
+
+async function logEvents(message = "something was logged") {
+  const date_time = new Intl.DateTimeFormat("en-us", {
+    year: "numeric",
+    month: "short",
+    weekday: "long",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  }).format(new Date());
+
+  const log_item = `${date_time}\t${crypto.randomUUID()}\t${message}\n`;
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", "event_log.txt"),
+      log_item
+    );
+  } catch (err) {
+    //handle error
+    console.log(err);
+  }
+}
+
+module.exports = logEvents;
